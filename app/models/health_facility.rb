be9385c05f644a6608b159c52587d48c8aa06f5e
erikathea <ethajes@gmail.com>
2015-09-08 12:58:41 +0800

    removed before validation

--- app/models/health_facility.rb
+++ app/models/health_facility.rb
@@ -1,7 +1,4 @@
 class HealthFacility < ActiveRecord::Base
   belongs_to :contact_detail
   validates :name, presence: true, uniqueness: true
-  before_validation do
-    self.name = name.titleize
-  end
 end
