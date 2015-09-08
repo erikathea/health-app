49e531c931e1025595ddd46192edf10a4478b6c5
erikathea <ethajes@gmail.com>
2015-09-09 01:04:28 +0800

    added more test scenarios for api calls with token auth

--- spec/lib/patient_api_spec.rb
+++ spec/lib/patient_api_spec.rb
@@ -35,8 +35,8 @@ describe API::PatientsController, type: :controller do
     end
   end
 
-  describe 'POST #save' do
-    context 'when adding valid data to patient' do
+  describe 'POST #save without TOKEN', :type => :api do
+    context 'when token is not present' do
       before(:each) do
         @patient = create(:patient)
         @lab_record_1 = create(:lab_record, patient: @patient)
@@ -46,6 +46,80 @@ describe API::PatientsController, type: :controller do
         @lab_detail_3 = create(:lab_detail, lab_record: @lab_record_2)
         post :save, payload,
           {'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s}
+      end
+
+      it 'returns unauthorized response' do
+        expect(response.status).to eq(401)
+      end
+
+      it 'shows a patient card' do
+        expect(response.body.strip).to eq('HTTP Token: Access denied.')
+      end
+    end
+  end
+
+  describe 'POST #save with TOKEN', :type => :api do
+    context 'when token is invalid' do
+      before(:each) do
+        @patient = create(:patient)
+        @lab_record_1 = create(:lab_record, patient: @patient)
+        @lab_record_2 = create(:lab_record, patient: @patient)
+        @lab_detail_1 = create(:lab_detail, lab_record: @lab_record_1)
+        @lab_detail_2 = create(:lab_detail, lab_record: @lab_record_1, normal_range: "0-10", unit: "u")
+        @lab_detail_3 = create(:lab_detail, lab_record: @lab_record_2)
+        post :save, payload,
+          {'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s, 'Authorization' => 'fake'}
+      end
+
+      it 'returns unauthorized response' do
+        expect(response.status).to eq(401)
+      end
+
+      it 'shows a patient card' do
+        expect(response.body.strip).to eq('HTTP Token: Access denied.')
+      end
+    end
+
+    context 'when token is expired' do
+      before(:each) do
+        @patient = create(:patient)
+        @lab_record_1 = create(:lab_record, patient: @patient)
+        @lab_record_2 = create(:lab_record, patient: @patient)
+        @lab_detail_1 = create(:lab_detail, lab_record: @lab_record_1)
+        @lab_detail_2 = create(:lab_detail, lab_record: @lab_record_1, normal_range: "0-10", unit: "u")
+        @lab_detail_3 = create(:lab_detail, lab_record: @lab_record_2)
+        api_key = create(:api_key)
+        api_key.update(created_at: api_key.created_at - 10.seconds)
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(api_key.access_token)
+        post :save, payload,
+          {'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s, 'Authorization' => 'fake'}
+      end
+
+      it 'returns unauthorized response' do
+        expect(response.status).to eq(401)
+      end
+
+      it 'shows a patient card' do
+        expect(response.body.strip).to eq('HTTP Token: Access denied.')
+      end
+    end
+
+    context 'when adding valid data to patient' do
+      before(:each) do
+        @patient = create(:patient)
+        @lab_record_1 = create(:lab_record, patient: @patient)
+        @lab_record_2 = create(:lab_record, patient: @patient)
+        @lab_detail_1 = create(:lab_detail, lab_record: @lab_record_1)
+        @lab_detail_2 = create(:lab_detail, lab_record: @lab_record_1, normal_range: "0-10", unit: "u")
+        @lab_detail_3 = create(:lab_detail, lab_record: @lab_record_2)
+        api_key = create(:api_key)
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(api_key.access_token)
+        post :save, payload,
+          {
+            'Accept' => Mime::JSON,
+            'Content-Type' => Mime::JSON.to_s,
+            'Authorization' => ActionController::HttpAuthentication::Token.encode_credentials(api_key.access_token)
+          }
       end
 
       it 'returns created status' do
