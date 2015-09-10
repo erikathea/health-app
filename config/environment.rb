c750449621bcdf3bbc346aa773724d833e8ba4f7
erikathea <ethajes@gmail.com>
2015-09-11 03:23:59 +0800

    added custom date format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Time::DATE_FORMATS[:formatted_datetime] = "%Y-%m-%d"
+Time::DATE_FORMATS[:date_conducted] = "%B %d, %Y"
